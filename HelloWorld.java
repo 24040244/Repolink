8e0331e659026b20889f2dad1354d0bb4ad9e83c
Thembelihle Sibanda <thembelihle.sibanda152@gmail.com>
2026-02-26 05:28:39 +0200

    my first commit

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.example.helloworld;
+
+/**
+ *
+ * @author themb
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+    }
+}
